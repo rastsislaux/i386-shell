6e16603d3d2edea4fe5cd28d4cd37c35df7ff4fa
Rastsislau Lipski <ungaf@ya.ru>
2022-12-13 02:25:43 +0300

    add commands

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,7 +6,7 @@
 #include "../drivers/keyboard.h"
 
 void main() {
-    print("Glory to Sobol Kernel!\n");
+    print("Welcome to Sable OS!\n");
     print("Installing interrupt service routines (ISRs)...\n");
     isr_install();
 
@@ -23,6 +23,17 @@ void execute_command(char *input) {
         print("Stopping the CPU. Bye!\n");
         asm volatile("hlt");
     }
+
+    if (strcmp(input, "SABLE") == 0) {
+        print("Glory to Sobol A. M.!\n> ");
+        return;
+    }
+
+    if (strcmp(input, "SABLEJR") == 0) {
+        print("Glory to Sobol A.M. and Tsiruk V. A.!\n> ");
+        return;
+    }
+
     print("Unknown command: ");
     print(input);
     print("\n> ");
